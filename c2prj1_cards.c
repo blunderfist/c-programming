f0e5bee43cb7fa4eecd195c4e1dd7d4da56f0601
blunderfist <75517840+blunderfist@users.noreply.github.com>
2021-03-18 13:24:29 -0700

    Update c2prj1_cards.c

--- c2prj1_cards.c
+++ c2prj1_cards.c
@@ -6,8 +6,8 @@ File Edit Options Buffers Tools C Help
 
 //check for valid value ranges of card                                                                                  
 void assert_card_valid(card_t c) {
-  assert((c.value >= 2) && (c.value <= VALUE_ACE));
-  assert((c.suit >= CLUBS) && (c.suit <= SPADES));
+  assert(c.value >= 2 && c.value <= VALUE_ACE);
+  assert(c.suit >= SPADES && c.suit <= CLUBS);
 }
 
 //rank hands                                                                                                            
@@ -22,7 +22,7 @@ const char * ranking_to_string(hand_ranking_t r) {
   case TWO_PAIR: return "TWO_PAIR";
   case PAIR: return "PAIR";
   case NOTHING: return "NOTHING";
-  default: return "NO_SUCH_COMBINATION";
+  default: return "";
   }
 }
 
@@ -61,35 +61,32 @@ void print_card(card_t c) {
   char c_value, c_suit;
   c_value = value_letter(c);
   c_suit = suit_letter(c);
-  printf("%c%c",c_value, c_suit);
+  printf("%c%c ",c_value, c_suit);
 }
 
 //make a card from 2 inputs                                                                                             
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
   switch(value_let){
-    case '2': temp.value = 2; break;
+  case '2': temp.value = 2; break;
   case '3': temp.value = 3; break;
   case '4': temp.value = 4; break;
   case '5': temp.value = 5; break;
   case '6': temp.value = 6; break;
   case '7': temp.value = 7; break;
   case '8': temp.value = 8; break;
-  case '9': temp.value = 9; break;  case '0': temp.value = 10; break;
+  case '9': temp.value = 9; break;
+  case '0': temp.value = 10; break;
   case 'J': temp.value = VALUE_JACK; break;
   case 'Q': temp.value = VALUE_QUEEN; break;
   case 'K': temp.value = VALUE_KING; break;
   case 'A': temp.value = VALUE_ACE; break;
   }
   switch(suit_let){
-  case 's': temp.suit = SPADES;
-    break;
-  case 'h': temp.suit = HEARTS;
-    break;
-  case 'd': temp.suit = DIAMONDS;
-    break;
-  case 'c': temp.suit = CLUBS;
-    break;
+  case 's': temp.suit = SPADES; break;
+  case 'h': temp.suit = HEARTS; break;
+  case 'd': temp.suit = DIAMONDS; break;
+  case 'c': temp.suit = CLUBS; break;
   }
   assert_card_valid(temp);
   return temp;
@@ -100,28 +97,27 @@ card_t card_from_num(unsigned c) {
   //assign a suit to value c based on number value                                                                      
   int v;
   int s;
-  v = c%13;
-  s = c/13;
+  //use % to find remainder, add 13 to if 0 or 1                                                                        
+  v = c % 13;
+  //4 sutis available, 0=s,1=h,2=d,3=c                                                                                  
+  s = c / 13;
 
   if (v >= 2) {
     temp.value = v;
-  }
+   }
   else {
     temp.value = v + 13;
   }
 
   switch(s){
-  case 0: temp.suit = SPADES;
-    break;
-  case 1: temp.suit = HEARTS;
-    break;
-  case 3: temp.suit = DIAMONDS;
-    break;
-  case 4: temp.suit = CLUBS;
-    break;
-  }
+  case 0: temp.suit = SPADES; break;
+  case 1: temp.suit = HEARTS; break;
+  case 2: temp.suit = DIAMONDS; break;
+  case 3: temp.suit = CLUBS; break;
+   }
 
   return temp;
+
 }
 
 
