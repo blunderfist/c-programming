461596dbb28bfea54b9c657e49b0a6da2bb051c6
blunderfist <75517840+blunderfist@users.noreply.github.com>
2021-04-26 23:48:37 -0700

    Update test-subseq.c

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -6,48 +6,43 @@ size_t maxSeq(int * array, size_t n);
 int main(void){
 
   int array1[] = {1, 2, 1, 3, 5, 7, 2, 4, 6, 9};
-  int array2[] = {-1, -2, -3, -4};
-  int array3[] = {-1};
-  int array4[] = {1,2,2,3};
-  int array5[] = {0};
-  int array6[] = {1,2,3,2,3,2,3,4,2,3,2,3,4,5,2,3,2};
-  int array7[] = {5,4,3,2};
+  int array2[] = {-1};
+  int array3[] = {1,2,2,3};
+  int array4[] = {5,-3,2,3};
+  int array5[] = {1,2,3,4};
+  int array6[] = {5,4,3,2};
 
-  if (maxSeq(array1, 10) != 4){
-    printf("Failed on array1");
-    return EXIT_FAILURE;
-  }
-  if (maxSeq(array2, 4) != 1){
-    printf("Failed array2");
-    return EXIT_FAILURE;
-  }
-  if (maxSeq(array3, 1) != 1){
-    printf("Failed array3");
+  if (maxSeq(NULL, 0) != 0 ){
+    printf("failed null\n");
     return EXIT_FAILURE;
   }
 
-  if (maxSeq(array4, 4) != 2){
-    printf("Failed array4");
+  if (maxSeq(array1, 10) != 4){
+    printf("Failed 1\n");
     return EXIT_FAILURE;
   }
+
+  if (maxSeq(array2, 1) != 1){
+    printf("failed 2\n");
+    return EXIT_FAILURE;
+  }
+
+  if (maxSeq(array3, 4) != 2){
+    printf("Failed 3");
+    return EXIT_FAILURE;
+   }
   
-    if (maxSeq(array5, 1) != 1){
-    printf("Failed array5");
+    if (maxSeq(array4, 4) != 3){
+    printf("failed 4\n");
+    return EXIT_FAILURE;
+   }
+
+  if (maxSeq(array5, 4) != 4){
+    printf("Failed 5");
     return EXIT_FAILURE;
   }
 
-  if (maxSeq(array6, 17) != 4){
-    printf("Failed array6");
-    return EXIT_FAILURE;
-  }
-
-  if (maxSeq(array7, 4) != 1){
-    printf("failed array7");
-    return EXIT_FAILURE;
-  }
-   
   else {
-    printf("Success\n");
     return EXIT_SUCCESS;
   }
 }
